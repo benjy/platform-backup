75124b58575cc25d8d9a477727acc3dda9f2764c
Ben Dougherty <ben@dougherty.net.au>
2017-07-16 11:37:40 +0800

    Fixed vendor include path.

--- db_backup.php
+++ db_backup.php
@@ -1,8 +1,7 @@
 <?php
 
-require_once './vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
-use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
 use Monolog\Handler\LogglyHandler;
 
