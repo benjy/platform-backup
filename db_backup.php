0ec2f1b980fae0599a2416a692105bdaac347aac
Ben Dougherty <ben@dougherty.net.au>
2017-07-27 11:07:54 +0800

    Support installing as a composer dependency.

--- db_backup.php
+++ db_backup.php
@@ -1,6 +1,12 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+// Support using this file as its own project or as composer requirement.
+foreach (array(__DIR__ . '/vendor/autoload.php', __DIR__ . '/../vendor/autoload.php', __DIR__ . '/../../autoload.php') as $file) {
+  if (file_exists($file)) {
+    require_once $file;
+    break;
+  }
+}
 
 use Monolog\Logger;
 use Monolog\Handler\LogglyHandler;
